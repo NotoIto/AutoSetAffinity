19749df288b0247c5e76e6211e3e07d9c0713a6f
NotoIto <notoito.public@gmail.com>
2020-12-17 02:19:19 +0900

    雛形の作成

---
+++ Presentation/ConfigModifyUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ConfigModifyUI : Window
+    {
+        public ConfigModifyUI()
+        {
+            InitializeComponent();
+        }
+        private void ToggleButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            var btn = (ToggleButton)sender;
+
+            btn.SetBinding(ToggleButton.IsCheckedProperty, new Binding("IsOpen") { Source = btn.ContextMenu });
+            btn.ContextMenu.PlacementTarget = btn;
+            btn.ContextMenu.Placement = PlacementMode.Bottom;
+        }
+    }
+}
